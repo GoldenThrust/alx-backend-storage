677dc766cf2b7d6e74543a7369194c56ff189a04
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-01-24 06:04:26 +0100

    school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,7 +4,6 @@
 
 def update_topics(mongo_collection, name, topics):
     """ Updates the topics in the collection """
-    query = mongo_collection.update_many(
+    mongo_collection.update_many(
         {"name": name}, {"$set": {"topics": topics}})
 
-    return query.modified_count
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 """ mongodb """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ return a list of instances of
+      the specified topic """
+    return mongo_collection.find({"topics": topic})
